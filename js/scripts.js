2d568b0bc09afb296a166c53aafae04918c1f304
eespinosa1305 <47625515+eespinosa1305@users.noreply.github.com>
2019-04-12 10:26:14 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
-/*$(document).ready(function() {
-	$menu = $('#menu').find('ul').find('li');
-
-	$menu.hover(function() {
-		$(this).children('ul').stop();
-		$(this).children('ul').slideDown();
-	}, function() {
-		$(this).children('ul').stop();
-		$(this).children('ul').slideUp();
-	});
-});*/
-
+$(function() {
+      $('#slides').slidesjs({
+        width: 940,
+        height: 210,
+        play: {
+          active: true,
+          auto: true,
+          interval: 4000,
+          swap: true
+        }
+      });
+    });
