76754bda8c314507e5189da446d2da2457593f03
eespinosa1305 <47625515+eespinosa1305@users.noreply.github.com>
2019-04-13 20:29:09 -0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
+/*$(document).ready(function() {
+	$menu = $('#menu').find('ul').find('li');
+
+	$menu.hover(function() {
+		$(this).children('ul').stop();
+		$(this).children('ul').slideDown();
+	}, function() {
+		$(this).children('ul').stop();
+		$(this).children('ul').slideUp();
+	});
+});*/
+
 $(function() {
       $('#slides').slidesjs({
         width: 940,
@@ -10,3 +22,31 @@ $(function() {
         }
       });
     });
+/*$(document).ready(function(){
+    $("#mostrar").on( "click", function() {
+      $('#target').show(3000); //muestro mediante id
+      $('.target').show(); //muestro mediante clase
+     });
+    $("#ocultar").on( "click", function() {
+      $('#target').hide(3000); //oculto mediante id
+      $('.target').hide(); //muestro mediante clase
+    });
+  });*/
+
+$(document).ready(function(){
+   $("#m1").click(function(){
+      $('#tabs-1').show(900);
+      $('#tabs-2').hide(900);
+      $('#tabs-3').hide(900);
+   });
+   $("#m2").click(function(){
+      $('#tabs-2').show(900);
+      $('#tabs-1').hide(900);
+      $('#tabs-3').hide(900);
+   });
+   $("#m3").click(function(){
+      $('#tabs-3').show(900);
+      $('#tabs-2').hide(900);
+      $('#tabs-1').hide(900);
+   });
+});
